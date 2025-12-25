b6e3c6ceb2cbd9bb323b09310c5bab2ddcf2e2e4
gabebonham <grotegabriel@gmail.com>
2025-12-24 22:15:01 -0300

    created about section

--- app/(public)/sobre/_components/AboutPage.tsx
+++ app/(public)/sobre/_components/AboutPage.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import AboutSection from './AboutSection'
 import ContactSection from './ContactSection'
 import FAQSection from './FAQSection'
 
 export default function AboutPage() {
   return (
     <div className="pt-42 px-4">
+      <AboutSection />
       <FAQSection />
       <ContactSection />
     </div>
--- constants/general-info.ts
+++ constants/general-info.ts
@@ -1,14 +1,11 @@
 export const generalInfo = {
   email: process.env.NEXT_PUBLIC_EMAIL!,
-  aboutText: `
-    A Biscuit Bijux nasceu do amor pelos detalhes, pelas cores e pela ideia de que cada acessório pode contar uma história. Somos uma loja apaixonada por bijuterias que valorizam a beleza, a criatividade e a autenticidade de cada pessoa.
-
-    Trabalhamos com peças cuidadosamente selecionadas (e criadas com carinho) para combinar com diferentes estilos e ocasiões — do dia a dia aos momentos especiais. Acreditamos que bijuterias vão além de simples acessórios: elas expressam personalidade, elevam a autoestima e transformam qualquer look.
-
-    Nosso compromisso é oferecer produtos de qualidade, com design atual, preço acessível e um atendimento próximo e atencioso. Cada cliente é único, e queremos que você se sinta confiante, especial e bem representada usando a Biscuit Bijux.
-
-    Seja bem-vinda(o) à nossa loja. Aqui, cada detalhe importa 💍✨
-    `,
+  aboutText: [
+    'A Biscuit Bijux nasceu do amor pelos detalhes, pelas cores e pela ideia de que cada acessório pode contar uma história. Somos uma loja apaixonada por bijuterias que valorizam a beleza, a criatividade e a autenticidade de cada pessoa.',
+    'Trabalhamos com peças cuidadosamente selecionadas (e criadas com carinho) para combinar com diferentes estilos e ocasiões — do dia a dia aos momentos especiais. Acreditamos que bijuterias vão além de simples acessórios: elas expressam personalidade, elevam a autoestima e transformam qualquer look.',
+    'Nosso compromisso é oferecer produtos de qualidade, com design atual, preço acessível e um atendimento próximo e atencioso. Cada cliente é único, e queremos que você se sinta confiante, especial e bem representada usando a Biscuit Bijux.',
+    'Seja bem-vinda(o) à nossa loja. Aqui, cada detalhe importa 💍✨',
+  ],
   aboutTextQuick:
     'Joias delicadas feitas com amor para mulheres que brilham. ✨',
   region: 'Porto Alegre, RS',
